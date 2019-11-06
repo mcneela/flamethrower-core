4e7b69cc258fcaeaeeeb41c8e10ab6db7113db01
mcneela <daniel.mcneela@gmail.com>
2019-11-06 11:52:46 -0800

    Add matmul grads and some other stuff

--- autograd/grad_defs.py
+++ autograd/grad_defs.py
@@ -58,11 +58,65 @@ define_grad(np.sqrt, 		lambda ans, g, x: g * 0.5 * x ** -0.5)
 define_grad(np.sinc, 		lambda ans, g, x: g * ((np.cos(np.pi * x) / x) - (np.sin(np.pi * x))/(np.pi * (x**2))))
 define_grad(np.reshape,		lambda ans, g, x, shape, order=None : np.reshape(g, np.shape(x), order=order))
 
-def grad_transpose(ans, x, g, axes=None):
+def grad_transpose(ans, g, x, axes=None):
 	if axes is not None:
 		axes = np.argsort(axes)
 	return np.transpose(g, axes)
 define_grad(np.transpose, grad_transpose)
 
+def unbroadcast(x, target_meta, broadcast_idx=0):
+	target_shape, target_ndim, dtype, target_iscomplex = target_meta
+	while np.ndim(x) > target_ndim:
+		x = np.sum(x, axis=broadcast_idx)
+	for axis, size in enumerate(target_shape):
+		if size == 1:
+			x = np.sum(x, axis=axis, keepdims=True)
+	if np.iscomplexobj(x) and not target_iscomplex:
+		x = np.real(x)
+	return x
 
+def matmul_adjoint_0(B, G, A_meta, B_ndim):
+	if np.ndim(G) == 0:  # A_ndim == B_ndim == 1
+		return unbroadcast(G * B, A_meta)
+	_, A_ndim, _, _ = A_meta
+	if A_ndim == 1:
+		G = np.expand_dims(G, np.ndim(G) - 1)
+	if B_ndim == 1:  # The result we need is an outer product
+		B = np.expand_dims(B, 0)
+		G = np.expand_dims(G, np.ndim(G))
+	else:  # We need to swap the last two axes of B
+		B = np.swapaxes(B, B_ndim - 2, B_ndim - 1)
+	result = np.matmul(G, B)
+	return unbroadcast(result, A_meta)
 
+def matmul_adjoint_1(A, G, A_ndim, B_meta):
+	if np.ndim(G) == 0:  # A_ndim == B_ndim == 1
+		return unbroadcast(G * A, B_meta)
+	_, B_ndim, _, _ = B_meta
+	B_is_vec = (B_ndim == 1)
+	if B_is_vec:
+		G = np.expand_dims(G, np.ndim(G))
+	if A_ndim == 1:  # The result we need is an outer product
+		A = np.expand_dims(A, 1)
+		G = np.expand_dims(G, np.ndim(G) - 1)
+	else:  # We need to swap the last two axes of A
+		A = np.swapaxes(A, A_ndim - 2, A_ndim - 1)
+	result = np.matmul(A, G)
+	if B_is_vec:
+		result = np.squeeze(result, np.ndim(G) - 1)
+	return unbroadcast(result, B_meta)
+
+def matmul_vjp_0(ans, g, A, B):
+	A_meta = metadata(A)
+	B_ndim = np.ndim(B)
+	return matmul_adjoint_0(B, g, A_meta, B_ndim)
+
+def matmul_vjp_1(ans, g, A, B):
+	A_ndim = np.ndim(A)
+	B_meta = metadata(B)
+	return matmul_adjoint_1(A, g, A_ndim, B_meta)
+
+def metadata(A):
+	return np.shape(A), np.ndim(A), np.result_type(A), np.iscomplexobj(A)
+
+define_grad(np.matmul, matmul_vjp_0, matmul_vjp_1)
--- autograd/testy.py
+++ autograd/testy.py
@@ -1,3 +1,4 @@
+import numpy as np
 from autograd import grad as gd, jacobian
 import tensor_library as tl
 from node import *
@@ -8,18 +9,20 @@ from core import *
 def fn(x):
 	z = 3 / x
 	y = 2 * x + z
-	print(z.data().shape)
-	print(y.data().shape)
+	# print(z.data().shape)
+	# print(y.data().shape)
 	q = z.T @ y
 	return q
 
 hand_grad = lambda x: 2 - 3 / (x ** 2)
 # start_node = GradNode.new_root()
 # my_grad = grad(fn)
-their_grad = gd(fn)
+their_grad = jacobian(fn)
 x = Tensor(tl.array([[4.0, 2.0, 3.0], [1.0, 7.0, 6.0]]))
+x2 = np.array([[4.0, 2.0, 3.0], [1.0, 7.0, 6.0]])
 y = fn(x)
+
 ten_grad = y.backward()
 # print("Calculated f'(3) = {}".format(my_grad(4.0)))
 # print("Expected   f'(3) = {}".format(hand_grad(4.0)))
-print("Their      f'(3) = {}".format(their_grad(4.0)))
+print("Their      f'(3) = {}".format(their_grad(x2)))
