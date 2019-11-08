d704b26899ed7c3e7b54275c6c0a818839118c6d
mcneela <daniel.mcneela@gmail.com>
2019-11-08 09:05:50 -0800

    Add some new initialization methods

--- flameflower/nn/initialize.py
+++ flameflower/nn/initialize.py
@@ -4,15 +4,66 @@ for neural network parameter initialization schemes.
 
 (C) 2019 MLephant, FlameFlower
 """
+from __future__ import division
 
 import flameflower.autograd.tensor_library as tl
 import flameflower.autograd.tensor_library.random as tlr
 
-def normalized_initialization(in_size, out_size, size=None):
+def get_in_out_dims(tensor):
+	"""
+	Gets the fan-in and fan-out
+	dimensions of the provided
+	tensor.
+	"""
+	dims = tensor.dims
+	if dims < 2:
+		raise ValueError("Tensor must have at least 2 dimensions.")
+
+	if dims == 2:
+		nin  = tensor.shape[1]
+		nout = tensor.shape[0]
+	else:
+		num_input_fmaps = tensor.shape[1]
+		num_output_fmaps = tensor.shape[0]
+		receptive_field_size = 1
+		if dims > 2:
+			receptive_field_size = tensor[0][0]._data.size
+		nin  = num_input_fmaps * receptive_field_size
+		nout = num_output_fmaps * receptive_field_size
+
+	return nin, nout
+
+def normalized(tensor):
 	"""
 	See Glorot and Bengio (2010)
 	Pg. 253, Eq. (16)
 	"""
-	high = tl.sqrt(6 / (in_size + out_size))
+	nin, nout = get_in_out_dims(tensor)
+	high = tl.sqrt(6 / float(nin + nout))
 	low = -high
-	return tlr.uniform(low, high, size=size)
+	return tlr.uniform(low, high, size=tensor.shape)
+
+def xavier_normal(tensor):
+	nin, nout = get_in_out_dims(tensor)
+	std = math.sqrt(2.0 / float(nin + nout))
+	return tlr.normal(scale=std, size=tensor.shape)
+
+def sparse(tensor, sparsity, std=0.01):
+	"""
+	Initialization method described in
+	"Deep Learning via Hessian-Free Optimization"
+	by Martens, J. (2010).
+	"""
+	if tensor.dims != 2:
+		raise ValueError("This initialization only works with Tensors having 2 dimensions.")
+
+	rows, cols = tensor.shape
+	num_zeros = int(math.ceil(sparsity * rows))
+
+	X = tlr.normal(loc=0, scale=std)
+	for col_idx in range(cols):
+		row_idxs = tlr.permutation(rows)
+		zero_idxs = row_idxs[:num_zeros]
+		X[zero_idxs, col_idx] = 0.
+	return X
+	
