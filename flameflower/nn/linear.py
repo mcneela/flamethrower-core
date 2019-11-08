2600926473860e60abffa23a175a19b3f477b889
mcneela <daniel.mcneela@gmail.com>
2019-11-08 08:23:17 -0800

    clean up linear module

--- flameflower/nn/linear.py
+++ flameflower/nn/linear.py
@@ -1,9 +1,10 @@
 from .module import Module
-import flameflower.autograd.tensor_library as np
 from flameflower.autograd import Tensor
-import numpy.random as npr
 
-npr.seed(0)
+import flameflower.autograd.tensor_library as tl
+import flameflower.autograd.tensor_library.random as tlr
+
+tlr.seed(0)
 class Linear(Module):
 	def __init__(self, in_size, out_size, use_bias=True):
 		super(Linear, self).__init__()
@@ -14,12 +15,12 @@ class Linear(Module):
 
 	def _init_params(self, init_fn=None):
 		if not init_fn:
-			init_fn = npr.randn
+			init_fn = tlr.randn
 		self.W = Tensor(init_fn(self.in_size, self.out_size))
-		self.b = Tensor(np.zeros((1, self.W.data().shape[1])))
+		self.b = Tensor(tl.zeros((1, self.W.data().shape[1])))
 		self.new_param('W', self.W)
 		if self.use_bias:
-			self.b = Tensor(np.ones((1, self.W.data().shape[1])))
+			self.b = Tensor(tl.ones((1, self.W.data().shape[1])))
 			self.new_param('b', self.b)
 
 	def forward(self, X):
