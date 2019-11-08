40d4990aa4d19179cd599ac73c9114364445d776
mcneela <daniel.mcneela@gmail.com>
2019-11-08 09:51:00 -0800

    add some docstrings to loss.py

--- flameflower/nn/loss.py
+++ flameflower/nn/loss.py
@@ -10,23 +10,46 @@ from __future__ import division
 import flameflower.autograd.tensor_library as tl
 
 def cross_entropy(y_hat, y):
+	"""
+	Cross-entropy loss, used for
+	classification with n classes.
+	"""
 	return -tl.sum(y * tl.log(y_hat))
 
 def binary_cross_entropy(y_hat, y):
+	"""
+	Binary cross-entropy loss, used
+	for classification with n classes.
+	"""
 	return -y * tl.log(y_hat) - (1 - y) * tl.log(1 - y_hat)
 
 def mean_squared_error(y_hat, y):
+	"""
+	MSE loss, often used for
+	regression tasks.
+	"""
 	n = len(y)
 	l = (1 / n) * tl.sum((y_hat - y) ** 2)
 	return l
 
 def l2(y_hat, y):
+	"""
+	Loss function using the L2 norm.
+	"""
 	return 0.5 * tl.sum((y_hat - y) ** 2)
 
 def l1(y_hat, y):
+	"""
+	Loss function using the L1 norm.
+	"""
 	return tl.abs(y_hat - y)
 
 def kl_divergence(p, q):
+	"""
+	Returns a notion of "distance"
+	between two probability distributions
+	p and q.
+	"""
 	return -tl.sum(p * tl.log(q / p))
 
 def huber(y_hat, y, delta=1):
