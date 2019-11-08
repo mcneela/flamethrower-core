50235842ebb47b8bd4254f13ad5b5e9b708861cd
mcneela <daniel.mcneela@gmail.com>
2019-11-07 19:10:57 -0800

    verify feedforward example with pytorch

--- examples/feedforward_pytorch.py
+++ examples/feedforward_pytorch.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-import flameflower.nn as nn
-import flameflower.optim as optim
-import flameflower.autograd.tensor_library as tl
-
-from flameflower.autograd import Tensor
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import matplotlib.pyplot as plt
 
 np.random.seed(0)
@@ -12,7 +10,7 @@ np.random.seed(0)
 X = np.linspace(-10, 10, num=100)
 y_hat = np.sin(X)
 
-X_train = Tensor(X)
+X_train = torch.Tensor(X)
 
 class Network(nn.Module):
 	def __init__(self, num_in, num_out):
@@ -21,37 +19,35 @@ class Network(nn.Module):
 		self.num_out = num_out
 		self.lin1 = nn.Linear(num_in, 5)
 		self.lin2 = nn.Linear(5, num_out)
-		self.add_module('linear1', self.lin1)
-		self.add_module('linear2', self.lin2)
 
-	def _forward(self, X):
+	def forward(self, X):
 		out = self.lin1(X)
-		out.node().name = 'W_1 @ X'
 		out = self.lin2(out)
-		out.node().name = 'W_2 @ X'
 		return out
 
 model = Network(1, 1)
-optimizer = optim.SGD(model.params(), lr=1e-4)
+optimizer = optim.SGD(model.parameters(), lr=1e-4)
 num_iters = 1000
 loss_fn = lambda y, y_hat: (y_hat - y) ** 2
 
 for i in range(num_iters):
 	print(i)
 	break
-	batch = Tensor([X[i % len(X)]])
+	optimizer.zero_grad()
+	batch = torch.Tensor([X[i % len(X)]])
 	y = model(batch)
-	loss = loss_fn(y, [y_hat[i % len(y_hat)]])
-	loss.node().name = '(y_hat - y)^2'
+	loss = loss_fn(y, torch.Tensor([y_hat[i % len(y_hat)]]))
+	# loss.node().name = '(y_hat - y)^2'
 	# loss.data().resize(100, 1)
 	loss.backward()
 	optimizer.step()
-	# optimizer.zero_grad()
 	# print("W1 grad is: {} ".format(model.lin1.W.node().grad))
 
 out = []
 for j in range(len(y_hat)):
-	z = model([X[j]]).data()[0]
+	z = model(torch.Tensor([X[j]])).data[0]
 	out.append(z)
 out = np.array(out)
 plt.plot(X, out)
+plt.show()
+
