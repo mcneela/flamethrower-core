7cb28c2f6f137f1da6590bced5dbfa18d19e750c
mcneela <daniel.mcneela@gmail.com>
2019-11-07 20:57:04 -0800

    update activations.py to use tl

--- flameflower/nn/activations.py
+++ flameflower/nn/activations.py
@@ -1,17 +1,16 @@
-# import autograd.numpy as np
-import numpy as np
+import flameflower.autograd.tensor_library as tl
 
 def sigmoid(x):
-	return 1 / (1 + np.exp(-x))
+	return 1 / (1 + tl.exp(-x))
 
 def softmax(x):
-	return np.exp / (np.sum(np.exp(x)))
+	return tl.exp / (tl.sum(tl.exp(x)))
 
 def relu(x):
-	return np.max(0, x)
+	return tl.max(0, x)
 
 def elu(x, alpha=1.0):
-	return relu(x) + np.min(0, alpha * (np.exp(x) - 1))
+	return relu(x) + tl.min(0, alpha * (tl.exp(x) - 1))
 
 def hardshrink(x, lval=0.5):
 	if x > lval or x < -lval:
@@ -19,35 +18,35 @@ def hardshrink(x, lval=0.5):
 	return 0
 
 def leaky_relu(x, alpha=0.1):
-	return relu(x) + alpha * np.min(0, x)
+	return relu(x) + alpha * tl.min(0, x)
 
 def relu6(x):
-	return np.min(np.max(0, x), 6)
+	return tl.min(tl.max(0, x), 6)
 
 def rrelu(x, lo=0.125, hi=1/3):
 	if x >= 0:
 		return x
-	return np.random.uniform(lo, hi) * x
+	return tl.random.uniform(lo, hi) * x
 
 def selu(x, alpha=1.6732632423543772848170429916717):
 	return 1.0507009873554804934193349852946 * elu(x, alpha=alpha)
 
 def celu(x, alpha=1.0):
-	return relu(x) + np.min(0, alpha * (np.exp(x / alpha) - 1))
+	return relu(x) + tl.min(0, alpha * (tl.exp(x / alpha) - 1))
 
 def softplus(x, beta=1, threshold=20):
 	if x <= threshold:
-		return (1 / beta) * np.log(1 + np.exp(beta * x))
+		return (1 / beta) * tl.log(1 + tl.exp(beta * x))
 	return x
 
 def softsign(x):
-	return x / (1 + np.abs(x))
+	return x / (1 + tl.abs(x))
 
 def tanh(x):
-	return np.tanh(x)
+	return tl.tanh(x)
 
 def tanhshrink(x):
-	return x - np.tanh(x)
+	return x - tl.tanh(x)
 
 def threshold(x, val):
 	if x > threshold:
@@ -55,10 +54,10 @@ def threshold(x, val):
 	return val
 
 def softmin(x):
-	return np.exp(-x) / (np.sum(np.exp(-x)))
+	return tl.exp(-x) / (tl.sum(tl.exp(-x)))
 
 def log_softmax(x):
-	return np.log(softmax(x))
+	return tl.log(softmax(x))
 
 def hardtanh(x, minx=-1, maxx=1, minv=-1, maxv=1):
 	if x > maxx:
@@ -74,7 +73,4 @@ def softshrink(x, lval=0.5):
 	elif x < -lval:
 		return x + lval
 	else:
-		return np.zeros(x.shape)
-
-
-
+		return tl.zeros(x.shape)
