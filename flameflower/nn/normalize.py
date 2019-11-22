780b97bd7d7d35fd50405cf3fee27cce66615b7c
mcneela <daniel.mcneela@gmail.com>
2019-11-21 17:11:28 -0800

    add beta and gamma as batch norm parameters

--- flameflower/nn/normalize.py
+++ flameflower/nn/normalize.py
@@ -7,15 +7,15 @@ class BatchNorm(Module):
 		super(BatchNorm, self).__init__()
 		self.eps = eps
 		self.gamma = Tensor(gamma)
+		self.new_param("gamma", self.gamma)
 		self.beta = Tensor(beta)
+		self.new_param("beta", self.beta)
 
 	def forward(self, X):
 		n = X.shape[0]
 		mu = tl.sum(X, axis=0) / n
-		print(f"Mu is: {mu.data}")
 		X - mu
 		std = tl.sum((X - mu) ** 2, axis=0) / n
 		x_hat = (X - mu) / tl.sqrt(std + self.eps)
 		y = self.gamma * x_hat + self.beta
-		print(y.shape)
 		return y
