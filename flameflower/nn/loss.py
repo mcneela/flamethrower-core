ee7a764d39e409196e3f35a3068a98d885da2324
mcneela <daniel.mcneela@gmail.com>
2019-12-06 16:55:51 -0800

    Add regularizer args

--- flameflower/nn/loss.py
+++ flameflower/nn/loss.py
@@ -25,16 +25,16 @@ def log_softmax(x, axis=None):
 	b = tl.max(x, axis=axis, keepdims=True)
 	return (x - b) - tl.log(tl.sum(tl.exp(x - b), axis=axis, keepdims=True))
 	
-def binary_cross_entropy(y_hat, y):
+def binary_cross_entropy(y_hat, y, eps=1e-5, regularizer=None):
 	"""
 	Binary cross-entropy loss, used
 	for classification with 2 classes.
 	"""
 	if regularizer is None:
 		regularizer = lambda: 0
-	return -y * tl.log(y_hat) - (1 - y) * tl.log(1 - y_hat) + regularizer()
+	return tl.mean(-y_hat * tl.log(y + eps) - (1 - y_hat) * tl.log(1 - y + eps) + regularizer())
 
-def mean_squared_error(y_hat, y):
+def mean_squared_error(y_hat, y, regularizer=None):
 	"""
 	MSE loss, often used for
 	regression tasks.
@@ -45,7 +45,7 @@ def mean_squared_error(y_hat, y):
 	l = (1 / n) * tl.sum((y_hat - y) ** 2)
 	return l + regularizer()
 
-def l2(y_hat, y):
+def l2(y_hat, y, regularizer=None):
 	"""
 	Loss function using the L2 norm.
 	Equivalent to minimization with
@@ -55,7 +55,7 @@ def l2(y_hat, y):
 		regularizer = lambda: 0
 	return 0.5 * tl.sum((y_hat - y) ** 2) + regularizer()
 
-def l1(y_hat, y):
+def l1(y_hat, y, regularizer=None):
 	"""
 	Loss function using the L1 norm.
 	"""
@@ -63,7 +63,7 @@ def l1(y_hat, y):
 		regularizer = lambda: 0
 	return tl.abs(y_hat - y) + regularizer()
 
-def kl_divergence(p, q):
+def kl_divergence(p, q, regularizer=None):
 	"""
 	Returns a notion of "distance"
 	between two probability distributions
@@ -73,7 +73,7 @@ def kl_divergence(p, q):
 		regularizer = lambda: 0
 	return -tl.sum(p * tl.log(q / p)) + regularizer()
 
-def huber(y_hat, y, delta=1):
+def huber(y_hat, y, delta=1, regularizer=None):
 	if regularizer is None:
 		regularizer = lambda: 0
 	if tl.abs(y_hat - y) < delta:
