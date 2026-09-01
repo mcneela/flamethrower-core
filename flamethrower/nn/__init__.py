89c802fdd2b38f7f0659c6e97965b30b01bc7e4b
Daniel McNeela <daniel.mcneela@gmail.com>
2026-09-01 19:58:37 -0400

    add new modules

--- flamethrower/nn/__init__.py
+++ flamethrower/nn/__init__.py
@@ -1,4 +1,6 @@
 from .module import Module
 from .linear import Linear
+from .batch_norm import BatchNorm
+from .regularize import Dropout
 
-__all__ = ['Module', 'Linear']
+__all__ = ['Module', 'Linear', 'BatchNorm', 'Dropout']
